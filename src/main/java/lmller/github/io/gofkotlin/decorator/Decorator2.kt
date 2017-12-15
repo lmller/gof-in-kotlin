8c066595f74353affeba593faaa3c68d98af35aa
Lovis Möller <lovis.moeller@codecentric.de>
2017-12-15 15:09:25 +0100

    add operator fun "plus" as alternative to "then"

--- src/main/java/lmller/github/io/gofkotlin/decorator/Decorator2.kt
+++ src/main/java/lmller/github/io/gofkotlin/decorator/Decorator2.kt
@@ -4,6 +4,8 @@ infix fun ((String) -> String).then(f: (String) -> String): (String) -> String {
     return { t -> f(this(t)) }
 }
 
+operator fun ((String) -> String).plus(f: (String) -> String) = then(f)
+
 infix fun String.decorate(f: (String) -> String): Text {
     return DefaultText(f(this))
 }
@@ -12,6 +14,6 @@ fun main(args: Array<String>) {
     fun underline(text: String) = "_${text}_"
     fun background(text: String) = "\u001B[43m$text\u001B[0m"
 
-    val text = "Functional?" decorate (::underline then ::background then ::underline)
+    val text = "Functional?" decorate (::underline then ::background + ::underline)
     text.draw()
 }
